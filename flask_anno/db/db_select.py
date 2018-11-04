a4b39f9a5913cfedbed173340320b3ef68ffccf6
mage1028 <524538131@qq.com>
2018-11-04 22:23:20 +0800

    0.2

--- flask_anno/db/db_select.py
+++ flask_anno/db/db_select.py
@@ -2,11 +2,13 @@ import pymysql
 import logging
 import json
 
-
+#
 # def connect():
 #     connect = pymysql.connect(host='localhost', user='root', password='', db='annotate2', port=3306,
 #                               charset='utf8')
 #     return connect
+
+
 def connect():
     connect = pymysql.connect(host='192.168.100.103', user='root', password='root', db='fakenewsb', port=3306,
                               charset='utf8')
